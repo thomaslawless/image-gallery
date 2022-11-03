9141f72f30293c8fe3c36868259ed7f77113c987
thomaslawless <lawlessptom@gmail.com>
2022-11-03 15:58:13 -0400

    spelling correctly

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -8,6 +8,7 @@ export default async function handler(
   //check for secret to confirm if valid
   if(req.query.secret !== process.env.REVALIDATE_SECRET){
     // testing
+    // spell things correctly
     return res.status(401).json({message: 'invalid token'})
   }
 
