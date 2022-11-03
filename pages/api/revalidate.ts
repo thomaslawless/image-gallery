168b2ab118336d6022ffad66961d55bc5fb30835
thomaslawless <lawlessptom@gmail.com>
2022-11-03 16:14:24 -0400

    does this work?

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -14,6 +14,7 @@ export default async function handler(
 
   try{
     //regenerate index route 
+    //updating 
     await res.revalidate('/')
     return res.json({revalidated: true})
   } catch (err){
