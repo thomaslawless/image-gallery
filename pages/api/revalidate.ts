fc6bd3b53ea684de0aa8ac0119db5addaa293eaf
thomaslawless <lawlessptom@gmail.com>
2022-11-03 15:43:38 -0400

    updating revalidate

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -12,7 +12,7 @@ export default async function handler(
 
   try{
     //regenerate index route 
-    await res.revalidate('/')
+    await res.revalidate('/index')
     return res.json({revalidated: true})
   } catch (err){
     return res.status(500).send('Error revalidating')
