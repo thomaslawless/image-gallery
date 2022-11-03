ed9fd4f84b6875783b1be750b2b9d6be2d3d5351
thomaslawless <lawlessptom@gmail.com>
2022-11-03 15:30:38 -0400

    add on demand isr

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+){
+  //check for secret to confirm if valid
+  if(req.query.secret !== process.env.REVALIDATE_SECRET){
+    return res.status(401).json({message: 'invalid token'})
+  }
+
+  try{
+    //regenerate index route 
+    await res.unstable_revalidate('/')
+    return res.json({revalidated: true})
+  } catch (err){
+    return res.status(500).send('Error revalidating')
+  }
+}
